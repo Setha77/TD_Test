d81719fd168ab5873cde9a57ecab9190692ecea3
Setha Hong <setha_hong77@hotmail.com>
2023-05-20 17:11:19 +0200

    implémentation des fonctions

--- CartePizzareria.py
+++ CartePizzareria.py
@@ -1,2 +1,26 @@
+class CartePizzeriaException(Exception):
+    pass
+
 class CartePizzareria:
+    def __init__(self):
+        self.listePizza = []
+
+    def getNbPizza(self):
+        return len(self.listePizza)
+    
+    def is_empty(self):
+        if len(self.listePizza) == 0:
+            print("La liste est vide")
+        else:
+            print("La liste n'est pas vide")
+    
+    def add_Pizza(self, pizza):
+        self.listePizza.append(pizza)
+    
+    def remove_Pizza(self, name):
+        try:
+            self.listePizza.remove(name)
+        except ValueError:
+            raise CartePizzeriaException(f"{name} n'a pas été trouvé")
+
     
